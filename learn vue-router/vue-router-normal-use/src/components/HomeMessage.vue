20bb48148034b7280732b61cbcf92fbc1b2d0b87
Hong <zehonghe0121@163.com>
2021-11-04 19:41:36 +0800

    unkonwn problem

--- learn vue-router/vue-router-normal-use/src/components/HomeMessage.vue
+++ learn vue-router/vue-router-normal-use/src/components/HomeMessage.vue
@@ -1,12 +1,12 @@
 <template>
-<div>
-  <ul>
-    <li>message1</li>
-    <li>message2</li>
-    <li>message3</li>
-    <li>message4</li>
-  </ul>
-</div>
+  <div>
+    <ul>
+      <li>message1</li>
+      <li>message2</li>
+      <li>message3</li>
+      <li>message4</li>
+    </ul>
+  </div>
 </template>
 
 <script>
